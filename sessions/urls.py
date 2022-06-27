1e808b3715ff53b37d1b10d793d029b4fe06e257
unknown <yashwanthkolli3271@gmail.com>
2022-06-27 19:50:09 +0530

    Barcode table

--- sessions/urls.py
+++ sessions/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home)
+    path('', views.home),
+    path('get/<str:id>',views.get_info),
+    path('__debug__/', include('debug_toolbar.urls'))
 ]
--- sessions/views.py
+++ sessions/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse
+import pandas as pd
 from .forms import SessionsForm
+from .models import Barcode
+import json
 
 # Create your views here.
 def home(req):
     issue_no = None
-    submitted = False
     form = SessionsForm()
     if req.method == "POST":
         form = SessionsForm(req.POST)
@@ -14,3 +16,19 @@ def home(req):
             issue_no = form.save().Issue_No
 
     return render(req, 'home.html', {'form': form, 'issue_no': issue_no})
+
+def model_to_dic(model):
+    dic={"message":"success"}
+    dic["product_name"]=model.Name
+    dic["item_number"]=model.Item_No
+    dic["barcodenumber"]=model.Barcode_No
+    return json.dumps(dic)
+
+def get_info(request,id):
+    
+    ans=Barcode.objects.filter(Barcode_No=id)
+    if(len(ans)==0):
+        return HttpResponse('{"message":"No objects found"}',content_type='application/json')
+    ans=model_to_dic(ans[0])
+
+    return HttpResponse(ans,content_type='application/json')
