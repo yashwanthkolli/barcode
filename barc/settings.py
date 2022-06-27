6f405b2896d39c23f85ec93a76ce19ea18981b51
unknown <yashwanthkolli3271@gmail.com>
2022-06-27 23:41:33 +0530

    Pdf

--- barc/settings.py
+++ barc/settings.py
@@ -37,12 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'sessions',
-    'debug_toolbar'
+    'sessions'
 ]
 
 MIDDLEWARE = [
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
